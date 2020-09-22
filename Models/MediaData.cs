d0fc5f4bad9178ff4e2e8de8955e3144efcc4e64
Manish Shukla <manishshukla@manishs-mbp.lan>
2020-09-22 11:16:22 +0100

    addded one more field in api to push images

--- Models/MediaData.cs
+++ Models/MediaData.cs
@@ -1,6 +1,8 @@
 public class MediaData {
-    public string Description{get;set;}
+   
     public string Mediabase64 {get;set;}
 
     public string fileExtension {get;set;}
+    public int Height {get;set;}
+    public int Width {get;set;}
 }
